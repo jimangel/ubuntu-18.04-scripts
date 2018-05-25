1ed2236e409b29228296074f1f3e6506e6fc15fd
Jim Angel <jameswangel@gmail.com>
2018-05-25 17:52:45 -0500

    adding script

---
+++ script.sh
@@ -0,0 +1,6 @@
+# curl -sL https://raw.githubusercontent.com/jimangel/ubuntu-tweaks/master/script.sh | sudo -E bash -
+
+# DO NOT DO THIS UNLESS YOUR ME
+
+# install tlp for better power mgmt on laptop
+sudo apt install tlp tlp-rdw --no-install-recommends
