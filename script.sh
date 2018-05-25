d2dad3f532665093ee464b509d979e28ad88148c
Jim Angel <jimangel@users.noreply.github.com>
2018-05-25 17:57:36 -0500

    added first steps

--- script.sh
+++ script.sh
@@ -1,3 +1,9 @@
+# DO FIRST...
+# sudo apt install git curl
+# git config --global user.email "<email>"
+# git config --global user.name "<name>"
+
+
 # curl -sL https://raw.githubusercontent.com/jimangel/ubuntu-tweaks/master/script.sh | sudo -E bash -
 
 # DO NOT DO THIS UNLESS YOUR ME
