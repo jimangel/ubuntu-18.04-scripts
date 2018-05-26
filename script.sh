67d90cd18c5041168d715ebbac6fb3ae12eb7ded
Jim Angel <jameswangel@gmail.com>
2018-05-25 20:08:55 -0500

    made more tweaks - stopping for now

--- script.sh
+++ script.sh
@@ -7,10 +7,8 @@
 # - install and setup go for golang
 # - install vscode
 
-# SETUP
-# sudo apt install git curl
-# git config --global user.email "<email>"
-# git config --global user.name "<name>"
+# SETUP & RUN
+# sudo apt install curl
 # curl -sL https://raw.githubusercontent.com/jimangel/ubuntu-tweaks/master/script.sh | sudo -E bash -
 
 # install tlp for better power mgmt on laptop
@@ -28,3 +26,21 @@ sudo apt-get install sublime-text
 
 # fix vi
 printf ":set nocompatible\n:set backspace=2" | tee ~/.vimrc > /dev/null
+
+# put dock at bottom
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+
+# git placeholder
+# sudo apt install git
+# git config --global user.email "<email>"
+# git config --global user.name "<name>"
+
+# install media codecs and fonts
+sudo apt install ubuntu-restricted-extras
+
+# update and upgrade all
+sudo apt update && sudo apt upgrade
+
+# reminder to update to AM/PM format
+echo "MANUALLY UPDATE TO A 12HR CLOCK"
+echo "STEPS: Click on Activities and search for 'Settings' and launch it. Click on Details at the bottom of the sidebar and then select 'Date & Time'."
