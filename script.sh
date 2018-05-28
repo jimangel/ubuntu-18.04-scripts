92417060360c8df3fdf83761d64f192d9d875aaf
Jim Angel <jameswangel@gmail.com>
2018-05-28 16:07:00 -0500

    adding more cloudnative junk

--- script.sh
+++ script.sh
@@ -86,6 +86,7 @@ cat << 'EOT' >> ~/.bashrc
 # set PATH so it includes go bin if it exists
 if [ -d "/usr/local/go/bin" ] ; then
     PATH="$PATH:/usr/local/go/bin"
+    GOPATH="$HOME/go/"
 fi
 EOT
 # create working dirs
@@ -104,7 +105,7 @@ if ! grep "source <(kubectl completion bash)" ~/.bashrc > /dev/null; then
 cat << 'EOT' >> ~/.bashrc
 
 # enable kubectl auto completion
-echo "source <(kubectl completion bash)"
+source <(kubectl completion bash)
 EOT
 fi
 
@@ -142,3 +143,30 @@ fi
 # add these to the config
 # "telemetry.enableTelemetry": false,
 # "telemetry.enableCrashReporter": false
+
+# install conjure-up for 'consure-up kubernetes' on vSphere
+#if cant_find_program conjure-up; then
+#sudo snap install conjure-up --classic
+# look into this
+#sudo snap install lxd
+#fi
+
+if cant_find_program gcloud; then
+# Create environment variable for correct distribution
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+# Add the Cloud SDK distribution URI as a package source
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+# Import the Google Cloud Platform public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+# Update the package list and install the Cloud SDK
+sudo apt update && sudo apt -y install google-cloud-sdk
+fi
+
+if cant_find_program cfssl; then
+wget -q --show-progress --https-only --timestamping \
+  https://pkg.cfssl.org/R1.2/cfssl_linux-amd64 \
+  https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
+  chmod +x cfssl_linux-amd64 cfssljson_linux-amd64
+  sudo mv cfssl_linux-amd64 /usr/local/bin/cfssl
+  sudo mv cfssljson_linux-amd64 /usr/local/bin/cfssljson
+fi
